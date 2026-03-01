6a341ffa68b92fca7351862f86d7bbf12573c0d3
Nafiz Mahmud Fardin <bsse1528@iit.du.ac.bd>
2026-03-02 01:09:06 +0600

    Modified

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ os.makedirs("extracted_frames/fake", exist_ok=True)
 os.makedirs("cropped_faces/real", exist_ok=True)
 os.makedirs("cropped_faces/fake", exist_ok=True)
 
-# Step 1: Extract frames
+
 process_videos("dataset/real", "extracted_frames/real", "real")
 process_videos("dataset/fake", "extracted_frames/fake", "fake")
 
-# Step 2: Detect faces
+
 detect_and_crop_faces("extracted_frames/real", "cropped_faces/real")
 detect_and_crop_faces("extracted_frames/fake", "cropped_faces/fake")
 
