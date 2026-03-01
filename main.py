6d5ed9a3131d75273005fb81edf3b187bf78b1f4
Nafiz Mahmud Fardin <bsse1528@iit.du.ac.bd>
2026-03-02 00:55:22 +0600

    mainfile

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from preprocess import process_videos, detect_and_crop_faces
+
+# Create required folders
+os.makedirs("extracted_frames/real", exist_ok=True)
+os.makedirs("extracted_frames/fake", exist_ok=True)
+os.makedirs("cropped_faces/real", exist_ok=True)
+os.makedirs("cropped_faces/fake", exist_ok=True)
+
+# Step 1: Extract frames
+process_videos("dataset/real", "extracted_frames/real", "real")
+process_videos("dataset/fake", "extracted_frames/fake", "fake")
+
+# Step 2: Detect faces
+detect_and_crop_faces("extracted_frames/real", "cropped_faces/real")
+detect_and_crop_faces("extracted_frames/fake", "cropped_faces/fake")
+
+print("Preprocessing Completed!")
